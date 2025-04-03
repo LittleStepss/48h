9b2b1190934edf2e40be076ec41d301bcc9b7ba9
luceMakaya <lucemakaya04@gmail.com>
2025-04-03 15:43:41 +0200

    feat: adding a api url

---
+++ front/lib/config/api.dart
@@ -0,0 +1,9 @@
+class APIConfig{
+  static const String baseUrl = "http://foureight.gurvan-nicolas.fr:8080";
+
+  // Endpoints
+  static const String clients = "$baseUrl/clients";
+  static const String individus = "$baseUrl/individus";
+  static const String scanCNI = "$baseUrl/cni";
+}
+
